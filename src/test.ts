c832bee62c4a4d7280aa7132af7e167720e4c099
LBF38 <contact.lbf38@gmail.com>
2023-06-22 15:47:13 +0200

    feat: :sparkles: introduce new api call feature (#3)

--- src/test.ts
+++ src/test.ts
@@ -3,3 +3,11 @@ async (test: string) => {
   return "a test";
 };
 console.log("Let's add a squash merge modification");
+async function apiCall(url: string) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+apiCall("https://api.github.com/users/octocat").then((data) => {
+  console.log(data);
+});
